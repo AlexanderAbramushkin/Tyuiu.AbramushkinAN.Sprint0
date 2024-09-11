76eef324463da7273e97e2736c3a0e178dd7d85e
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-11 13:43:11 +0500

    Task2

---
+++ Tyuiu.AbramushkinAN.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.AbramushkinAN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.AbramushkinAN.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Александр";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Александр", res);
+        }
+    }
+}
